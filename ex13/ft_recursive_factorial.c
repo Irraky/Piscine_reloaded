121a50f084aeb62348960eda3515e22146ccfc50
Déborah <deborah.recourse@hotmail.fr>
2016-11-09 23:17:57 +0100

    ex13

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -1,17 +1,14 @@
 int	ft_recursive_factorial(int nb)
 {	
-	int i;
-
-	i = nb;
-	if (i < 0)
+	if (nb < 0)
 		return(0);
-	else if (i == 0)
+	else if (nb == 0)
 		return(1);
-	else if (i == 1)
+	else if (nb == 1)
 		nb = 1;
 	else
 	{	
-		--i;
+		--nb;
 		nb = nb * ft_recursive_factorial(nb - 1);
 	}
 	return (nb);
