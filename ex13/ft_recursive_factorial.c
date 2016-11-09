0fd0398a9f9360e71703ef3cd3ebc80a69e0ec22
Déborah <deborah.recourse@hotmail.fr>
2016-11-09 23:12:01 +0100

    coin

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -3,11 +3,13 @@ int	ft_recursive_factorial(int nb)
 	int i;
 
 	i = nb;
-	if (i <= 0)
+	if (i < 0)
 		return(0);
-	if (i == 1)
+	else if (i == 0)
+		return(1);
+	else if (i == 1)
 		nb = 1;
-	if (i > 1)
+	else
 	{	
 		--i;
 		nb = nb * ft_recursive_factorial(nb - 1);
